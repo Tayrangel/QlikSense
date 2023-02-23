db11311657dcb2e943893998b246f1531eb6e9ea
Tayhana Marçal Rangel da Fonseca <85115577+Tayrangel@users.noreply.github.com>
2023-02-22 21:46:49 -0300

    Extensão de KPI com preenchimento

---
+++ Extensão/figura_com_preenchimento.js
@@ -0,0 +1,72 @@
+/*globals define*/
+define( ["qlik", "jquery", "text!./style.css"], function ( qlik, $, cssContent ) {
+	'use strict';
+	$( "<style>" ).html( cssContent ).appendTo( "head" );
+	function createRows ( rows, dimensionInfo ) {
+		var html = "";
+		rows.forEach( function ( row ) {
+			html += '<tr>';
+			row.forEach( function ( cell, key ) {
+				if ( cell.qIsOtherCell ) {
+					cell.qText = dimensionInfo[key].othersLabel;
+				}
+				html += "<td ";
+				if ( !isNaN( cell.qNum ) ) {
+					html += "class='numeric'";
+				}
+				html += '>' + cell.qText + '</td>';
+			} );
+			html += '</tr>';
+		} );
+		return html;
+	}
+
+	return {
+		initialProperties: {
+			qHyperCubeDef: {
+				qDimensions: [],
+				qMeasures: [],
+				qInitialDataFetch: [{
+					qWidth: 10,
+					qHeight: 50
+				}]
+			}
+		},
+		definition: {
+			type: "items",
+			component: "accordion",
+			items: {
+				dimensions: {
+					uses: "dimensions",
+					min: 0
+				},
+				measures: {
+					uses: "measures",
+					min: 0
+				},
+				sorting: {
+					uses: "sorting"
+				},
+				settings: {
+					uses: "settings"
+				}
+			}
+		},
+		snapshot: {
+			canTakeSnapshot: true
+		},
+		paint: function ( $element, layout ) {
+			var numero = layout.qHyperCube.qDataPages[0].qMatrix[0][0].qText;
+			
+			//console.log(layout);
+			var html = '';
+			
+			html += '<div class="pai">';
+			html += '<div class="img"></div>';
+			html += '<div class="legenda"> '+ numero +' </div>';
+			html += '</div>';
+			
+			$element.html(html);
+		}
+	};
+} );
